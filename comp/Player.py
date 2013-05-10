650597f8e5d7bcc1d298b95cf6ab7978d0861380
Buford Peek <bpeek@lancope.com>
2013-05-10 14:16:42 -0400

    initial commit

---
+++ comp/Player.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+class Player():
+	def __init__(self):
+		pass
---
+++ comp/Team.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+class Team():
+
+	def __init__(self):
+		pass
